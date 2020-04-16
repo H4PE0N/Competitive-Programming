911fa4493bded461c2be56a09606898cd5fe6217
H4PE0N <h4pe0n.pyton.3.6@gmail.com>
2020-04-16 15:59:23 +0200

    Generated some more functions

--- competitive-function-program.h
+++ competitive-function-program.h
@@ -2,6 +2,8 @@
 #ifndef COMPETITIVE_FUNCTION_PROGRAM_H
 #define COMPETITIVE_FUNCTION_PROGRAM_H
 
+/* A collection of competitive functions*/
+
 int** generate_matrix_array(int, int);
 int** allocate_matrix_values(int**, int*);
 
@@ -11,5 +13,18 @@ int* allocate_array_values(int*, int);
 int* bubble_sort_array(int*, const int);
 int* bubble_sort_iteration(int*, int);
 int* switch_array_numbers(int*, int);
+
+int** generate_integer_hashlist(const int);
+int calculate_integer_amount(int*, int*);
+
+int calculate_hashvalue_index(int**, int);
+int calculate_hashlist_length(int**, int);
+int hashlist_value_exists(int**, int);
+
+int** increment_hashlist_value(int**, int);
+int** generate_hashlist_value(int**, int);
+
+int** allocate_hashlist_value(int**, int);
+int** allocate_array_hashlist(int*, int);
 
 #endif
