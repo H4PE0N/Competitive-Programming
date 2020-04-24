70ea7c607235fdab505e2df76f8ea80500b81989
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-04-24 23:17:18 +0200

    Created an array and allocated numbers to it.

--- competitive-program.c
+++ competitive-program.c
@@ -21,6 +21,8 @@ Competitive-Function-Program-3/competitive-function-program-3.c -lm */
 
 int main(int argc, char** argv)
 {
+  int* array = generate_empty_array(10);
+  array = (int[]) {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   return 0;
 }
 
