460743717eb739f618f45f9bd58791678603191f
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-05-01 21:14:40 +0200

    Sorted all the files and folders so that you can access something much faster.

--- competitive-program.c
+++ competitive-program.c
@@ -4,24 +4,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "Competitive-Function-Program-1/\
-competitive-function-program-1.h"
+#include "Competitive-Functions-Program-1/\
+competitive-functions-program-1.h"
 
-#include "Competitive-Function-Program-2/\
-competitive-function-program-2.h"
+#include "Competitive-Functions-Program-2/\
+competitive-functions-program-2.h"
 
-#include "Competitive-Function-Program-3/\
-competitive-function-program-3.h"
+#include "Competitive-Functions-Program-3/\
+competitive-functions-program-3.h"
 
-/* Compile program by typing:
+#include "Competitive-Functions-Program-4/\
+competitive-functions-program-4.h"
+
+#include "Competitive-Functions-Program-5/\
+competitive-functions-program-5.h"
+
+/* Compile the program with gcc by typing:
+
 gcc -o competitive-program competitive-program.c \
-Competitive-Function-Program-1/competitive-function-program-1.c \
-Competitive-Function-Program-2/competitive-function-program-2.c \
-Competitive-Function-Program-3/competitive-function-program-3.c -lm */
+Competitive-Functions-Program-1/competitive-functions-program-1.c \
+Competitive-Functions-Program-2/competitive-functions-program-2.c \
+Competitive-Functions-Program-3/competitive-functions-program-3.c \
+Competitive-Functions-Program-4/competitive-functions-program-4.c \
+Competitive-Functions-Program-5/competitive-functions-program-5.c -lm
+
+*/
 
 int main(int argc, char** argv)
 {
   return 0;
 }
 
-/* Made by Roy Hampus Fridholm - 2020/04/15 */
+/* Made by Roy Hampus Fridholm */
