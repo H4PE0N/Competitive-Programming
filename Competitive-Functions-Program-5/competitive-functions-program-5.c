2277f6bcef8307f1ac5ff58f6d7c45ade1c7b5ff
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-05-03 11:25:11 +0200

    I have done functions on the 5th program that converts between binary, ascii and decimal.

--- Competitive-Functions-Program-5/competitive-functions-program-5.c
+++ Competitive-Functions-Program-5/competitive-functions-program-5.c
@@ -2,6 +2,81 @@
 /*  This program contains functions that deals with */
 
 #include <stdlib.h>
-#include <stdbool.h>
+#include <math.h>
+
+#include "../Competitive-Functions-Program-4/\
+competitive-functions-program-4.h"
+
+int reduce_binary_decimal(char* binary, int length,
+  int decimal)
+{
+  int weight = length - calculate_string_length(binary);
+  if(decimal - pow(2, weight) >= 0)
+  { decimal = decimal - pow(2, weight); } return decimal;
+}
+
+char* allocate_binary_value(char* binary, int length,
+  int decimal)
+{
+  int index = calculate_string_length(binary), weight =
+  (length - 1) - index;
+  if(decimal - pow(2, weight) >= 0)
+  { binary = add_string_character(binary, index, '1'); }
+  else
+  { add_string_character(binary, index, '0'); }
+  return binary;
+}
+
+char* allocate_binary_values(char* binary, int length,
+  int decimal)
+{
+  for(int index = 0; index < length; index++)
+  {
+    binary = allocate_binary_value(binary,length,decimal);
+    decimal = reduce_binary_decimal(binary, length,
+      decimal);
+  }
+  return binary;
+}
+
+int calculate_binary_length(const int decimal)
+{
+  int length = 0; while(decimal > pow(2, length))
+  { length = (length + 1); } return length;
+}
+
+char* convert_decimal_binary(const int decimal)
+{
+  int length = calculate_binary_length(decimal);
+  char* binary = generate_empty_string(length);
+  return allocate_binary_values(binary, length, decimal);
+}
+
+char* convert_character_binary(char character)
+{
+  return convert_decimal_binary(character);
+}
+
+int calculate_decimal_binary(char* binary, int index,
+  int decimal)
+{
+  int length = calculate_string_length(binary);
+  if(*(binary + index) == '1')
+  {
+    return decimal + pow(2, (length - (index + 1) ));
+  }
+  return decimal;
+}
+
+int convert_binary_decimal(char* binary)
+{
+  int length = calculate_string_length(binary),decimal=0;
+  for(int index = 0; index < length; index++)
+  {
+    decimal = calculate_decimal_binary(binary, index,
+      decimal);
+  }
+  return decimal;
+}
 
 /* Made by Roy Hampus Fridholm */
--- Competitive-Functions-Program-5/competitive-functions-program-5.h
+++ Competitive-Functions-Program-5/competitive-functions-program-5.h
@@ -5,7 +5,16 @@ with */
 #ifndef COMPETITIVE_FUNCTIONS_PROGRAM__H
 #define COMPETITIVE_FUNCTIONS_PROGRAM__H
 
+char* convert_decimal_binary(const int);
+char* convert_character_binary(char);
+int convert_binary_decimal(char*);
 
+char* allocate_binary_values(char*, int);
+char* allocate_binary_value(char*, int, int);
+
+int reduce_binary_decimal(char*, int, int);
+int calculate_binary_length(const int);
+int calculate_decimal_binary(char*, int, int);
 
 #endif
 
