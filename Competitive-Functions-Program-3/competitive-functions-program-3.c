9b2bf1c8352f20e9d933e7204424872b0a0ec91a
H4PE0N <h4pe0n.pyton.3.6@gmail.com>
2020-05-04 12:34:14 +0200

    Renamed functions with hashlist to hashmap. And then removed the bucket comment from the bucket-list file.

--- Competitive-Functions-Program-3/competitive-functions-program-3.c
+++ Competitive-Functions-Program-3/competitive-functions-program-3.c
@@ -1,6 +1,6 @@
 
 /*  This program contains functions that deals with
-hashlists and manipulation of them. */
+hashmaps and manipulation of them. */
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,7 +11,7 @@ competitive-functions-program-1.h"
 #include "../Competitive-Functions-Program-2/\
 competitive-functions-program-2.h"
 
-int** generate_integer_hashlist(const int length)
+int** generate_integer_hashmap(const int length)
 {
   return generate_matrix_array(length, 2);
 }
@@ -26,11 +26,11 @@ int calculate_integer_amount(int* array, int* info)
   return amount;
 }
 
-int hashlist_value_exists(int** hashlist, int value)
+int hashmap_value_exists(int** hashmap, int value)
 {
-  int index = 0; while(*(*(hashlist + index) + 1) != 0)
+  int index = 0; while(*(*(hashmap + index) + 1) != 0)
   {
-    if(*(*(hashlist + index)) == value){return true;}
+    if(*(*(hashmap + index)) == value){return true;}
     index++;
   }
   return false;
@@ -46,54 +46,54 @@ int calculate_hashvalue_index(int** list, int value)
   return -1;
 }
 
-int calculate_hashlist_length(int** hashlist)
+int calculate_hashmap_length(int** hashmap)
 {
-  int index = 0; while(*(*(hashlist + index) + 1) != 0)
+  int index = 0; while(*(*(hashmap + index) + 1) != 0)
   { index++; } return index;
 }
 
-int** increment_hashlist_value(int** list, int value)
+int** increment_hashmap_value(int** list, int value)
 {
   int index = calculate_hashvalue_index(list, value);
   *(*(list + index) + 1) += 1; return list;
 }
 
-int** generate_hashlist_value(int** list, int value)
+int** generate_hashmap_value(int** list, int value)
 {
-  int length = calculate_hashlist_length(list);
+  int length = calculate_hashmap_length(list);
   *(*(list + length) + 0) = value;
   *(*(list + length) + 1) = 1; return list;
 }
 
-int** allocate_hashlist_value(int** list, int value)
+int** allocate_hashmap_value(int** list, int value)
 {
-  if(!hashlist_value_exists(list, value))
+  if(!hashmap_value_exists(list, value))
   {
-    list = generate_hashlist_value(list, value);
+    list = generate_hashmap_value(list, value);
   } else {
-    list = increment_hashlist_value(list, value);
+    list = increment_hashmap_value(list, value);
   }
   return list;
 }
 
-int** allocate_array_hashlist(int* array, int length)
+int** allocate_array_hashmap(int* array, int length)
 {
-  int** hashlist = generate_integer_hashlist(length);
+  int** hashmap = generate_integer_hashmap(length);
   for(int index = 0; index < length; index++)
   {
-    hashlist = allocate_hashlist_value(
-      hashlist, *(array + index));
+    hashmap = allocate_hashmap_value(
+      hashmap, *(array + index));
   }
-  return hashlist;
+  return hashmap;
 }
 
-int* hashlist_keyword_array(int** hashlist)
+int* hashmap_keyword_array(int** hashmap)
 {
-  int length = calculate_hashlist_length(hashlist);
+  int length = calculate_hashmap_length(hashmap);
   int* array = generate_empty_array(length);
   for(int index = 0; index < length; index++)
   {
-    *(array + index) = *(*(hashlist + index) + 0);
+    *(array + index) = *(*(hashmap + index) + 0);
   }
   return array;
 }
