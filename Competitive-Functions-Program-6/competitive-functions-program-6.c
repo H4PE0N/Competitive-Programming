f42d1cee66740fff0d25ccb30ac1c999dc59ca5b
H4PE0N <h4pe0n.pyton.3.6@gmail.com>
2020-05-05 09:23:21 +0200

    Wrote some functions that calculates Average, Median, Typical (6).

--- Competitive-Functions-Program-6/competitive-functions-program-6.c
+++ Competitive-Functions-Program-6/competitive-functions-program-6.c
@@ -11,20 +11,35 @@ competitive-functions-program-1.h"
 #include "../Competitive-Functions-Program-3/\
 competitive-functions-program-3.h"
 
-int calculate_array_average(int* array, const int length)
-{
-  int array_total = 0;
-  for(int index = 0; index < length; index++)
-  {
-    array_total = (array_total + *(array + index));
-  }
-  return (array_total / length);
-}
-
-int calculate_array_median(int* array, const int length)
+double calculate_odd_median(int* array, int length)
 {
   array = bubble_sort_array(array, length);
-  return *(array + (int) floor(length / 2) );
+  return *(array + ( (length - 1) / 2) );
+}
+
+double calculate_even_median(int* array, int length)
+{
+  array = bubble_sort_array(array, length);
+  double first = (double) *(array + ((length-2) / 2)),
+  second = (double) *(array + ( length / 2));
+  return ( ( second + first) / 2);
+}
+
+double calculate_array_median(int* array, int length)
+{
+  if(length % 2 != 0) // check if length is odd;
+  { return calculate_odd_median(array, length); }
+  else { return calculate_even_median(array, length); }
+}
+
+double calculate_array_average(int* array, int length)
+{
+  double array_total = 0;
+  for(int index = 0; index < length; index++)
+  {
+    array_total += (double) *(array + index);
+  }
+  return (array_total / length);
 }
 
 int calculate_common_value(int** hashmap, int length)
@@ -39,7 +54,7 @@ int calculate_common_value(int** hashmap, int length)
   return *(*(hashmap + common_index) + 0);
 }
 
-int calculate_array_typical(int* array, const int length)
+int calculate_array_typical(int* array, int length)
 {
   int** hashmap = allocate_array_hashmap(array,length);
   int hash_length = calculate_hashmap_length(hashmap);
