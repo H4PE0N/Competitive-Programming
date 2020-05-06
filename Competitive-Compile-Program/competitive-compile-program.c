25036382d8969b277fe2cad623860b0c0dade133
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-05-06 21:20:48 +0200

    Made a compile instructions program and renamed the compile program.

--- Competitive-Compile-Program/competitive-compile-program.c
+++ Competitive-Compile-Program/competitive-compile-program.c
@@ -25,21 +25,9 @@ competitive-functions-program-6.h"
 #include "../Competitive-Functions-Program-7/\
 competitive-functions-program-7.h"
 
-/* Compile the program with gcc by typing:
-
-gcc -o competitive-program competitive-program.c \
-../Competitive-Functions-Program-1/competitive-functions-program-1.c \
-../Competitive-Functions-Program-2/competitive-functions-program-2.c \
-../Competitive-Functions-Program-3/competitive-functions-program-3.c \
-../Competitive-Functions-Program-4/competitive-functions-program-4.c \
-../Competitive-Functions-Program-5/competitive-functions-program-5.c \
-../Competitive-Functions-Program-6/competitive-functions-program-6.c \
-../Competitive-Functions-Program-7/competitive-functions-program-7.c -lm
-
-*/
-
 int main(int argc, char** argv)
 {
+  printf("%s\n", "Write your code here!");
   return 0;
 }
 
