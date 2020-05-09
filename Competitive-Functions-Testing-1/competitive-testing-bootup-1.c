4f85ca1dbae8e1bcfbeacf0ae81e331cd59229cc
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-05-09 22:59:54 +0200

    Made a program that is going to boot up the tests.

---
+++ Competitive-Functions-Testing-1/competitive-testing-bootup-1.c
@@ -0,0 +1,21 @@
+
+/*  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../Competitive-Functions-Program-1/\
+competitive-functions-program-1.h"
+
+#include "../Competitive-Functions-Testing-1/\
+competitive-functions-testing-1.h"
+
+/* gcc -o competitive-testing-bootup-1\
+competitive-testing-bootup-1.c */
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
+
+/* Made by Roy Hampus Fridholm */
