b421e129c6ee9b31bbc026210555859737c52061
H4PE0N <h4pe0n.pyton.3.6@gmail.com>
2020-05-14 21:22:45 +0200

    Removed comments and t_length to length in file 7.

--- Competitive-Program-Folder/Competitive-Functions-Program-7/competitive-functions-program-7.c
+++ Competitive-Program-Folder/Competitive-Functions-Program-7/competitive-functions-program-7.c
@@ -5,26 +5,24 @@
 #include "../Competitive-Functions-Program-4/\
 competitive-functions-program-4.h"
 
-// Inputs: binary, total length;
-char* binary_left_shifting(char* binary, int t_length)
+char* binary_left_shifting(char* binary, int length)
 {
-  char* new_binary = generate_empty_string(t_length);
-  for(int index = 1; index < t_length; index++)
+  char* new_binary = generate_empty_string(length);
+  for(int index = 1; index < length; index++)
   {
     new_binary = add_string_character(new_binary,
       index - 1, *(binary + index));
   }
-  return add_string_character(new_binary, t_length-1,
+  return add_string_character(new_binary, length-1,
     *(binary + 0));
 }
 
-// Inputs: binary, total length;
-char* binary_right_shifting(char* binary, int t_length)
+char* binary_right_shifting(char* binary, int length)
 {
-  char* new_binary = generate_empty_string(t_length);
+  char* new_binary = generate_empty_string(length);
   new_binary = add_string_character(new_binary, 0,
-    *(binary + t_length - 1));
-  for(int index = 0; index < (t_length - 1); index++)
+    *(binary + length - 1));
+  for(int index = 0; index < (length - 1); index++)
   {
     new_binary = add_string_character(new_binary,
       (index + 1), *(binary + index));
@@ -32,12 +30,11 @@ char* binary_right_shifting(char* binary, int t_length)
   return new_binary;
 }
 
-// Inputs: first binary, second binary, total length;
 char* binary_and_opperation(char* first, char* second,
-  int t_length)
+  int length)
 {
-  char* new_binary = generate_empty_string(t_length);
-  for(int index = 0; index < t_length; index++)
+  char* new_binary = generate_empty_string(length);
+  for(int index = 0; index < length; index++)
   {
     if(*(first + index)=='1' && *(second + index)=='1')
     { new_binary =
@@ -48,12 +45,11 @@ char* binary_and_opperation(char* first, char* second,
   return new_binary;
 }
 
-// Inputs: first binary, second binary, total length;
 char* binary_or_opperation(char* first, char* second,
-  int t_length)
+  int length)
 {
-  char* new_binary = generate_empty_string(t_length);
-  for(int index = 0; index < t_length; index++)
+  char* new_binary = generate_empty_string(length);
+  for(int index = 0; index < length; index++)
   {
     if(*(first + index)=='1' || *(second + index)=='1')
     { new_binary =
@@ -64,12 +60,11 @@ char* binary_or_opperation(char* first, char* second,
   return new_binary;
 }
 
-// Inputs: first binary, second binary, total length;
 char* binary_xor_opperation(char* first, char* second,
-  int t_length)
+  int length)
 {
-  char* new_binary = generate_empty_string(t_length);
-  for(int index = 0; index < t_length; index++)
+  char* new_binary = generate_empty_string(length);
+  for(int index = 0; index < length; index++)
   {
     if(*(first + index) != *(second + index))
     { new_binary =
@@ -80,11 +75,10 @@ char* binary_xor_opperation(char* first, char* second,
   return new_binary;
 }
 
-// Inputs: binary, total length;
-char* binary_not_opperation(char* binary, int t_length)
+char* binary_not_opperation(char* binary, int length)
 {
-  char* new_binary = generate_empty_string(t_length);
-  for(int index = 0; index < t_length; index++)
+  char* new_binary = generate_empty_string(length);
+  for(int index = 0; index < length; index++)
   {
     if(*(binary + index) == '1') { new_binary =
       add_string_character(new_binary, index, '0'); }
--- Competitive-Program-Folder/Competitive-Functions-Program-7/competitive-functions-program-7.h
+++ Competitive-Program-Folder/Competitive-Functions-Program-7/competitive-functions-program-7.h
@@ -2,28 +2,19 @@
 #ifndef COMPETITIVE_FUNCTIONS_PROGRAM_7_H
 #define COMPETITIVE_FUNCTIONS_PROGRAM_7_H
 
-// Inputs: binary, total length;
 char* binary_left_shifting(char*, int);
 
-// Inputs: binary, total length;
 char* binary_right_shifting(char*, int);
 
-/* Inputs: first binary, second binary,
-total length; */
 char* binary_and_opperation(char*,char*,
   int);
 
-/* Inputs: first binary, second binary,
-total length; */
 char* binary_or_opperation(char*,char*,
   int);
 
-/* Inputs: first binary, second binary,
-total length; */
 char* binary_xor_opperation(char*,char*,
   int);
 
-// Inputs: binary, total length;
 char* binary_not_opperation(char*,int);
 
 #endif
