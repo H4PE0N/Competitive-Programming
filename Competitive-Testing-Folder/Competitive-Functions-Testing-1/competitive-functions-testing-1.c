583e883715a157e6b28c9335eb61105d26061903
H4PE0N <h4pe0n.pyton.3.6@gmail.com>
2020-05-14 21:28:44 +0200

    Got rid of comments in test file 1.

--- Competitive-Testing-Folder/Competitive-Functions-Testing-1/competitive-functions-testing-1.c
+++ Competitive-Testing-Folder/Competitive-Functions-Testing-1/competitive-functions-testing-1.c
@@ -5,8 +5,6 @@
 Competitive-Functions-Program-1/\
 competitive-functions-program-1.h"
 
-/* Inputs: input array, index, integer,
-output array; */
 int allocate_array_integer_test(int* i_array,
   int index, int integer, int* o_array)
 {
@@ -17,124 +15,105 @@ int allocate_array_integer_test(int* i_array,
     length);
 }
 
-/* Inputs: input array, total length, index,
-output array; */
 int delete_array_integer_test(int* i_array,
-  int t_length, int index, int* o_array)
+  int length, int index, int* o_array)
 {
-  i_array = delete_array_integer(i_array, t_length,
+  i_array = delete_array_integer(i_array, length,
     index);
   return compare_integer_arrays(i_array, o_array,
-    t_length);
+    length);
 }
 
-// Inputs: input array, total length, output array;
 int allocate_array_integers_test(int* i_array,
-  int t_length, int* o_array)
+  int length, int* o_array)
 {
   i_array = allocate_array_integers(i_array,
-    t_length);
+    length);
   return compare_integer_arrays(i_array, o_array,
-    t_length);
+    length);
 }
 
-// Inputs: total length, output array;
-int generate_integer_array_test(int t_length,
+int generate_integer_array_test(int length,
   int* o_array)
 {
-  int* array = generate_integer_array(t_length);
+  int* array = generate_integer_array(length);
   return compare_integer_arrays(array, o_array,
-    t_length);
+    length);
 }
 
-// Inputs: input array, length;
 int integer_array_length_test(int* i_array,
-  int length)
+  int i_length)
 {
-  int o_length = integer_array_length(i_array);
-  return (o_length == length);
+  int length = integer_array_length(i_array);
+  return (length == i_length);
 }
 
-/* Inputs: input array, total length, integer,
-boolean; */
 int array_contains_integer_test(int* i_array,
-  int t_length, int integer, int boolean)
+  int length, int integer, int boolean)
 {
-  int output=array_contains_integer(i_array,
-    t_length, integer); return (output == boolean);
+  int output = array_contains_integer(i_array,
+    length, integer); return (output == boolean);
 }
 
-// Inputs: input array, index, output array;
 int switch_array_integers_test(int* i_array,
   int index, int* o_array)
 {
   i_array = switch_array_integers(i_array, index);
   int length = integer_array_length(i_array);
-  return compare_integer_arrays(i_array ,o_array,
+  return compare_integer_arrays(i_array, o_array,
     length);
 }
 
-// Inputs: input array, index, output array;
 int sort_integer_iteration_test(int* i_array,
   int index, int* o_array)
 {
   i_array = sort_integer_iteration(i_array, index);
   int length = integer_array_length(i_array);
-  return compare_integer_arrays(i_array ,o_array,
+  return compare_integer_arrays(i_array, o_array,
     length);
 }
 
-/* Inputs: input array, current length,
-output array; */
 int sort_integer_array_test(int* i_array,
-  int c_length, int* o_array)
+  int length, int* o_array)
 {
-  i_array = sort_integer_array(i_array, c_length);
+  i_array = sort_integer_array(i_array, length);
   return compare_integer_arrays(i_array, o_array,
-    c_length);
+    length);
 }
 
-/* Inputs: input array, current length, integer,
-output array; */
 int add_array_integer_test(int* i_array,
-  int c_length, int integer, int* o_array)
+  int length, int integer, int* o_array)
 {
-  i_array = add_array_integer(i_array, c_length,
+  i_array = add_array_integer(i_array, length,
     integer);
-  c_length = integer_array_length(i_array);
+  length = integer_array_length(i_array);
   return compare_integer_arrays(i_array, o_array,
-    c_length);
+    length);
 }
 
-/* Inputs: input array, current length, start,
-output array; */
 int move_array_integers_test(int* i_array,
-  int c_length, int start, int* o_array)
+  int length, int start, int* o_array)
 {
-  i_array = move_array_integers(i_array,c_length,
+  i_array = move_array_integers(i_array, length,
     start);
-  return compare_integer_arrays(i_array,o_array,
-    c_length);
+  return compare_integer_arrays(i_array, o_array,
+    length);
 }
 
-/* Inputs: input array, current length, integer,
-output array; */
 int remove_array_integer_test(int* i_array,
-  int c_length, int integer, int* o_array)
+  int length, int integer, int* o_array)
 {
-  i_array = remove_array_integer(i_array,c_length,
+  i_array = remove_array_integer(i_array, length,
     integer);
-  return compare_integer_arrays(i_array,o_array,
-    c_length);
+  return compare_integer_arrays(i_array, o_array,
+    length);
 }
 
-// Inputs: input array, integer, output array;
 int remove_array_integers_test(int* i_array,
   int integer, int* o_array)
 {
-  i_array = remove_array_integers(i_array,
-    integer);
-  int c_length = integer_array_length(i_array);
+  i_array = remove_array_integers(i_array, integer);
+  int length = integer_array_length(i_array);
   return compare_integer_arrays(i_array, o_array,
-    c_length);
+    length);
 }
--- Competitive-Testing-Folder/Competitive-Functions-Testing-1/competitive-functions-testing-1.h
+++ Competitive-Testing-Folder/Competitive-Functions-Testing-1/competitive-functions-testing-1.h
@@ -2,52 +2,30 @@
 #ifndef COMPETITIVE_FUNCTIONS_TESTING_1_H
 #define COMPETITIVE_FUNCTIONS_TESTING_1_H
 
-/* Inputs: input array, index, integer,
-output array; */
-int allocate_array_integer_test(int*, int, int,
-  int*);
+int allocate_array_integer_test(int*,int,int,int*);
 
-/* Inputs: input array, total length, index,
-output array; */
 int delete_array_integer_test(int*,int,int,int*);
 
-/* Inputs: input array, total length,
-output array; */
 int allocate_array_integers_test(int*, int, int*);
 
-// Inputs: total length, output array;
 int generate_integer_array_test(int, int*);
 
-// Inputs: input array, length;
 int integer_array_length_test(int*, int);
 
-/* Inputs: input array, total length, integer,
-boolean; */
 int array_contains_integer_test(int*,int,int,int);
 
-// Inputs: input array, index, output array;
 int switch_array_integers_test(int*, int, int*);
 
-// Inputs: input array, index, output array;
 int sort_integer_iteration_test(int*, int, int*);
 
-/* Inputs: input array, current length, output
-array; */
 int sort_integer_array_test(int*, int, int*);
 
-/* Inputs: input array, current length, integer,
-output array; */
 int add_array_integer_test(int*, int, int, int*);
 
-/* Inputs: input array, current length, start,
-output array; */
 int move_array_integers_test(int*,int,int,int*);
 
-/* Inputs: input array, current length, integer,
-output array; */
 int remove_array_integer_test(int*,int,int,int*);
 
-// Inputs: input array, integer, output array;
 int remove_array_integers_test(int*, int, int*);
 
 #endif
