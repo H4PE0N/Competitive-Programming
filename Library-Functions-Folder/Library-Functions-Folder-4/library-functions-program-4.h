84e8014311d3cfa644aca98a0999db88f7ed4b4d
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-05-30 17:27:28 +0200

    Changed the program 4 header file, it lookes better.

--- Library-Functions-Folder/Library-Functions-Folder-4/library-functions-program-4.h
+++ Library-Functions-Folder/Library-Functions-Folder-4/library-functions-program-4.h
@@ -2,26 +2,35 @@
 #ifndef LIBRARY_FUNCTIONS_PROGRAM_4_H
 #define LIBRARY_FUNCTIONS_PROGRAM_4_H
 
-char* allocate_string_character(char*,int,char);
+char* allocate_string_character(char*,
+  int, char);
 
-char* allocate_string_characters(char*,int);
+char* allocate_string_characters(char*,
+  int);
 
 char* generate_empty_string(int);
 
 int calculate_string_length(char*);
 
-int string_contains_character(char*,int,char);
+int string_contains_character(char*,
+  int, char);
 
-char* switch_string_characters(char*, int);
+char* switch_string_characters(char*,
+  int);
 
-char* move_string_characters(char*, int, int);
+char* move_string_characters(char*,
+  int, int);
 
-char* remove_string_character(char*,int,char);
+char* remove_string_character(char*,
+  int,char);
 
-char* add_string_character(char*, int, char);
+char* add_string_character(char*,int,
+  char);
 
-int compare_strings_together(char*,char*,int);
+int compare_strings_together(char*,
+  char*, int);
 
-char* remove_string_characters(char*,int,char);
+char* remove_string_characters(char*,
+  int, char);
 
 #endif
