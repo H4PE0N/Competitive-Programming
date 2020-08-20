6544a77fde651daa0d4c6284b8c63f8aba471c85
Hampus Fridholm <hampus.fridholm@gmail.com>
2020-08-20 21:19:20 +0200

    Removed include statement to folder 2 from folder 5 (all files).

--- Functions-Testing-Folder/Functions-Testing-Folder-5/functions-testing-program-5-2.c
+++ Functions-Testing-Folder/Functions-Testing-Folder-5/functions-testing-program-5-2.c
@@ -4,10 +4,6 @@ Library-Functions-Folder-1/\
 library-functions-program-1.h"
 
 #include "../../Library-Functions-Folder/\
-Library-Functions-Folder-2/\
-library-functions-program-2.h"
-
-#include "../../Library-Functions-Folder/\
 Library-Functions-Folder-4/\
 library-functions-program-4.h"
 
