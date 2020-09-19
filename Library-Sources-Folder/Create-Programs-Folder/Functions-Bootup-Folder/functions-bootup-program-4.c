4a84e934cb61926f66b4c73ddf38f70252dbb1f7
Hampus Fridholm <hampus.fridholm@gmail.com>
2020-09-19 13:14:28 +0200

    Implemented header files in the bootup programs.

--- Library-Sources-Folder/Create-Programs-Folder/Functions-Bootup-Folder/functions-bootup-program-4.c
+++ Library-Sources-Folder/Create-Programs-Folder/Functions-Bootup-Folder/functions-bootup-program-4.c
@@ -4,48 +4,10 @@
 #include <time.h>
 
 #include "../../Library-Functions-Folder/\
-Library-Functions-Folder-1/\
-library-functions-program-1.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-2/\
-library-functions-program-2.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-3/\
-library-functions-program-3.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-4/\
-library-functions-program-4.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-5/\
-library-functions-program-5.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-6/\
-library-functions-program-6.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-7/\
-library-functions-program-7.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-8/\
-library-functions-program-8.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-9/\
-library-functions-program-9.h"
-
-#include "../../Library-Functions-Folder/\
-Library-Functions-Folder-10/\
-library-functions-program-10.h"
+library-functions-headers.h"
 
 #include "../../Functions-Testing-Folder/\
-Functions-Testing-Folder-4/\
-functions-testing-program-4.h"
+functions-testing-headers.h"
 
 int main(int argc, char** argv)
 {
