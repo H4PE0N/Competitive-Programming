938cb68c8161ca14f8006531e826a03e2642bff5
Hampus Fridholm <hampus.fridholm@gmail.com>
2020-09-19 12:48:23 +0200

    Made include files that include every file in folder.

---
+++ Library-Sources-Folder/Functions-Testing-Folder/functions-testing-headers.h
@@ -0,0 +1,38 @@
+
+#ifndef FUNCTIONS_TESTING_HEADERS_H
+#define FUNCTIONS_TESTING_HEADERS_H
+
+#include "../Library-Functions-Folder/\
+library-functions-headers.h"
+
+#include "Functions-Testing-Folder-1/\
+functions-testing-program-1.h"
+
+#include "Functions-Testing-Folder-2/\
+functions-testing-program-2.h"
+
+#include "Functions-Testing-Folder-3/\
+functions-testing-program-3.h"
+
+#include "Functions-Testing-Folder-4/\
+functions-testing-program-4.h"
+
+#include "Functions-Testing-Folder-5/\
+functions-testing-program-5.h"
+
+#include "Functions-Testing-Folder-6/\
+functions-testing-program-6.h"
+
+#include "Functions-Testing-Folder-7/\
+functions-testing-program-7.h"
+
+#include "Functions-Testing-Folder-8/\
+functions-testing-program-8.h"
+
+#include "Functions-Testing-Folder-9/\
+functions-testing-program-9.h"
+
+#include "Functions-Testing-Folder-10/\
+functions-testing-program-10.h"
+
+#endif
---
+++ Library-Sources-Folder/Library-Functions-Folder/library-functions-headers.h
@@ -0,0 +1,35 @@
+
+#ifndef LIBRARY_FUNCTIONS_HEADERS_H
+#define LIBRARY_FUNCTIONS_HEADERS_H
+
+#include "Library-Functions-Folder-1/\
+library-functions-program-1.h"
+
+#include "Library-Functions-Folder-2/\
+library-functions-program-2.h"
+
+#include "Library-Functions-Folder-3/\
+library-functions-program-3.h"
+
+#include "Library-Functions-Folder-4/\
+library-functions-program-4.h"
+
+#include "Library-Functions-Folder-5/\
+library-functions-program-5.h"
+
+#include "Library-Functions-Folder-6/\
+library-functions-program-6.h"
+
+#include "Library-Functions-Folder-7/\
+library-functions-program-7.h"
+
+#include "Library-Functions-Folder-8/\
+library-functions-program-8.h"
+
+#include "Library-Functions-Folder-9/\
+library-functions-program-9.h"
+
+#include "Library-Functions-Folder-10/\
+library-functions-program-10.h"
+
+#endif
